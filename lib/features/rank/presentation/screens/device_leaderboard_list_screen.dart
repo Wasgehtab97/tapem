e90ae187d2878cd192ae1464b449f6f9731e7bd6
Daniel <dmisztal.1297@gmail.com>
2025-06-25 05:27:29 +0200

    newest push

--- lib/features/rank/presentation/screens/device_leaderboard_list_screen.dart
+++ lib/features/rank/presentation/screens/device_leaderboard_list_screen.dart
@@ -1,3 +1,5 @@
+// lib/features/rank/presentation/screens/device_leaderboard_list_screen.dart
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tapem/core/providers/gym_provider.dart';
@@ -24,9 +26,10 @@ class DeviceLeaderboardListScreen extends StatelessWidget {
             title: Text(d.name),
             subtitle: Text('Code: ${d.id}'),
             onTap: () {
-              Navigator.of(
-                context,
-              ).pushNamed(AppRouter.rankScreen, arguments: gymId);
+              Navigator.of(context).pushNamed(
+                AppRouter.rank, // <-- hier angepasst
+                arguments: gymId,
+              );
             },
           );
         },
