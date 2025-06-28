2e72216922e4abbc56cb438ef9409af14aa9536c
Wasgehtab97 <dmisztal.1297@gmail.com>
2025-06-28 05:14:18 +0200

    Filter training plans by creator

--- lib/features/training_plan/domain/repositories/training_plan_repository.dart
+++ lib/features/training_plan/domain/repositories/training_plan_repository.dart
@@ -1,6 +1,6 @@
 import '../models/training_plan.dart';
 
 abstract class TrainingPlanRepository {
-  Future<List<TrainingPlan>> getPlans(String gymId);
+  Future<List<TrainingPlan>> getPlans(String gymId, String userId);
   Future<void> savePlan(String gymId, TrainingPlan plan);
 }
