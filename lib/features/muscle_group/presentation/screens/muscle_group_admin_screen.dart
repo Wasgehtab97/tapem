d59c08153f4f717271b6a955183b61bc010ad8e6
Wasgehtab97 <dmisztal.1297@gmail.com>
2025-08-11 15:22:54 +0200

    fix(ui): resolve SearchBar import conflict

--- lib/features/muscle_group/presentation/screens/muscle_group_admin_screen.dart
+++ lib/features/muscle_group/presentation/screens/muscle_group_admin_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide SearchBar;
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:tapem/core/providers/auth_provider.dart';
