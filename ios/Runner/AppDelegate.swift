4527f85e79ec502d773f09434ae8ff57845d513a
Wasgehtab97 <dmisztal.1297@gmail.com>
2025-08-19 10:12:52 -0700

    chore: prepare iOS app for TestFlight

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,26 @@
 import Flutter
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import FirebaseAppCheck
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()
+    AppCheck.setAppCheckProviderFactory(AppCheckDebugProviderFactory())
+    UNUserNotificationCenter.current().delegate = self
+    application.registerForRemoteNotifications()
+    Messaging.messaging().delegate = self
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("FCM token: \(fcmToken ?? "")")
   }
 }
